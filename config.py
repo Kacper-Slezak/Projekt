971ff96e086ed059533d64494a684c3dfc76b758
Kacper-Slezak <422843@student.agh.edu.pl>
2025-03-28 12:22:57 +0100

    End

--- config.py
+++ config.py
@@ -6,5 +6,12 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'domyslny-klucz')
     WTF_CSRF_SECRET_KEY = os.getenv('WTF_CSRF_SECRET_KEY', 'domyslny-klucz-csrf')
-    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'uploads')
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf'}
+    
+    # Use an absolute path for UPLOAD_FOLDER
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
+    
+    # Ensure the upload directory exists
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'pdf', 'txt'}
